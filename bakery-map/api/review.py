a2edc56136e14a4f281d2121526defd11dcd87f9
SunHee <76764643+sunni546@users.noreply.github.com>
2024-06-23 23:19:42 +0900

    feat: 빵집 id를 통한 빵집의 전체 리뷰(reviews) GET API
    (+) refactor: 리뷰 작성(삭제) 시, 사용자 포인트 증가(감소)하면서 사용자의 레벨 갱신

--- bakery-map/api/review.py
+++ bakery-map/api/review.py
@@ -2,8 +2,8 @@ from flask import request, jsonify
 from flask_restx import Namespace, Resource
 
 from api.bakery import add_review_score, subtract_review_score, change_review_score, get_bakery_name
-from api.user import plus_point, minus_point
-from models import Review, db
+from api.user import plus_point, minus_point, get_user_level, get_user_nickname
+from models import Review, db, User
 from my_jwt import validate_token, get_user_id
 
 Review_api = Namespace(name='Review_api', description="API for managing reviews")
@@ -55,8 +55,9 @@ class ReviewCR(Resource):
 
         try:
             reviews = Review.query.filter_by(user_id=user_id).all()
+
             for review in reviews:
-                result.append(make_result(review))
+                result.append(make_result(review, 1))
 
         except Exception as e:
             print(e)
@@ -109,17 +110,16 @@ class ReviewCR(Resource):
             else:
                 review = Review(content=content, image=image, score=score, bakery_id=bakery_id, user_id=user_id)
 
+                add_review_score(bakery_id, score)
+                plus_point(user_id, 10)
+
                 db.session.add(review)
+                db.session.commit()
 
                 from api.reviewed_bread import set_category_in_reviewed_breads
                 set_category_in_reviewed_breads(review.id, category_ids)
 
-                add_review_score(bakery_id, score)
-                plus_point(user_id, 10)
-
-                db.session.commit()
-
-                result = make_result(review)
+                result = make_result(review, 1)
 
         except Exception as e:
             print(e)
@@ -146,6 +146,8 @@ class ReviewRUD(Resource):
               "score": 5,
               "bakery_id": 1,
               "bakery_name": "파리바게뜨 부천중동로데오점",
+              "user_nickname": "nickname1",
+              "user_level": "초심자",
               "breads": [
                 "베이글"
               ]
@@ -276,7 +278,124 @@ class ReviewRUD(Resource):
         return jsonify(result)
 
 
-def make_result(review):
+@Review_api.route('/bakery/<int:id>')
+@Review_api.doc(params={'id': 'Bakery ID'})
+class ReviewRC(Resource):
+    def get(self, id):
+        """
+          Get all reviews with bakery ID.
+        """
+        """
+          Request:
+            GET /reviews/bakery/1
+          Returns:
+            [
+              {
+                "id": 3,
+                "content": "소금빵 좋아요",
+                "image": "review3",
+                "score": 1,
+                "user_nickname": "nickname2",
+                "user_level": "초심자",
+                "breads": [
+                  "소금빵"
+                ]
+              },
+              {
+                "id": 1,
+                "content": "베이글 좋아요",
+                "image": "review1",
+                "score": 5,
+                "user_nickname": "nickname1",
+                "user_level": "초심자",
+                "breads": [
+                  "베이글"
+                ]
+              },
+              ...
+            ]
+        """
+        token = request.headers.get('Authorization')
+
+        if not validate_token(token):
+            return jsonify({'result': "로그인 실패", 'message': "올바르지 않은 JWT입니다."})
+
+        print(id)
+
+        result = []
+
+        try:
+            reviews = Review.query.filter_by(bakery_id=id).order_by(Review.id.desc()).all()
+
+            for review in reviews:
+                result.append(make_result(review, -1))
+
+        except Exception as e:
+            print(e)
+
+        return jsonify(result)
+
+    def post(self, id):
+        """
+          Get all reviews with bakery ID and level ID.
+        """
+        """
+          Request:
+            POST /reviews/bakery/1
+            {
+              "level_id": 1
+            }
+          Returns:
+            [
+              {
+                "id": 3,
+                "content": "소금빵 좋아요",
+                "image": "review3",
+                "score": 1,
+                "user_nickname": "nickname2",
+                "user_level": "초심자",
+                "breads": [
+                  "소금빵"
+                ]
+              },
+              {
+                "id": 1,
+                "content": "베이글 좋아요",
+                "image": "review1",
+                "score": 5,
+                "user_nickname": "nickname1",
+                "user_level": "초심자",
+                "breads": [
+                  "베이글"
+                ]
+              },
+              ...
+            ]
+        """
+        token = request.headers.get('Authorization')
+
+        if not validate_token(token):
+            return jsonify({'result': "로그인 실패", 'message': "올바르지 않은 JWT입니다."})
+
+        level_id = request.json.get('level_id')
+        print(level_id)
+
+        result = []
+
+        try:
+            reviews = (Review.query.join(Review.user).filter(Review.bakery_id == id).filter(User.level_id == level_id)
+                       .order_by(Review.id.desc()).all())
+
+            for review in reviews:
+                result.append(make_result(review, -1))
+
+        except Exception as e:
+            print(e)
+
+        return jsonify(result)
+
+
+def make_result(review, k=0):
     from api.reviewed_bread import get_category_names_in_reviewed_breads
 
     result = {
@@ -284,9 +403,15 @@ def make_result(review):
         'content': review.content,
         'image': review.image,
         'score': review.score,
-        'bakery_id': review.bakery_id,
-        'bakery_name': get_bakery_name(review.bakery_id),
         'breads': get_category_names_in_reviewed_breads(review.id)
     }
+
+    if k >= 0:
+        result['bakery_id'] = review.bakery_id
+        result['bakery_name'] = get_bakery_name(review.bakery_id)
+
+    if k < 1:
+        result['user_nickname'] = get_user_nickname(review.user_id)
+        result['user_level'] = get_user_level(review.user_id)
 
     return result
